ca57702ad4c03fd182790c6d515daea0f24b60af
filtered <176114999+webfiltered@users.noreply.github.com>
2024-10-12 16:07:06 +1100

    nit - Format

--- src/LiteGraphGlobal.ts
+++ src/LiteGraphGlobal.ts
@@ -24,152 +24,152 @@ export class LiteGraphGlobal {
     SlotType = SlotType
     LabelPosition = LabelPosition
 
-    VERSION = 0.4;
+    VERSION = 0.4
 
-    CANVAS_GRID_SIZE = 10;
+    CANVAS_GRID_SIZE = 10
 
-    NODE_TITLE_HEIGHT = 30;
-    NODE_TITLE_TEXT_Y = 20;
-    NODE_SLOT_HEIGHT = 20;
-    NODE_WIDGET_HEIGHT = 20;
-    NODE_WIDTH = 140;
-    NODE_MIN_WIDTH = 50;
-    NODE_COLLAPSED_RADIUS = 10;
-    NODE_COLLAPSED_WIDTH = 80;
-    NODE_TITLE_COLOR = "#999";
-    NODE_SELECTED_TITLE_COLOR = "#FFF";
-    NODE_TEXT_SIZE = 14;
-    NODE_TEXT_COLOR = "#AAA";
-    NODE_SUBTEXT_SIZE = 12;
-    NODE_DEFAULT_COLOR = "#333";
-    NODE_DEFAULT_BGCOLOR = "#353535";
-    NODE_DEFAULT_BOXCOLOR = "#666";
-    NODE_DEFAULT_SHAPE = "box";
-    NODE_BOX_OUTLINE_COLOR = "#FFF";
-    DEFAULT_SHADOW_COLOR = "rgba(0,0,0,0.5)";
-    DEFAULT_GROUP_FONT = 24;
+    NODE_TITLE_HEIGHT = 30
+    NODE_TITLE_TEXT_Y = 20
+    NODE_SLOT_HEIGHT = 20
+    NODE_WIDGET_HEIGHT = 20
+    NODE_WIDTH = 140
+    NODE_MIN_WIDTH = 50
+    NODE_COLLAPSED_RADIUS = 10
+    NODE_COLLAPSED_WIDTH = 80
+    NODE_TITLE_COLOR = "#999"
+    NODE_SELECTED_TITLE_COLOR = "#FFF"
+    NODE_TEXT_SIZE = 14
+    NODE_TEXT_COLOR = "#AAA"
+    NODE_SUBTEXT_SIZE = 12
+    NODE_DEFAULT_COLOR = "#333"
+    NODE_DEFAULT_BGCOLOR = "#353535"
+    NODE_DEFAULT_BOXCOLOR = "#666"
+    NODE_DEFAULT_SHAPE = "box"
+    NODE_BOX_OUTLINE_COLOR = "#FFF"
+    DEFAULT_SHADOW_COLOR = "rgba(0,0,0,0.5)"
+    DEFAULT_GROUP_FONT = 24
     DEFAULT_GROUP_FONT_SIZE?: any
 
-    WIDGET_BGCOLOR = "#222";
-    WIDGET_OUTLINE_COLOR = "#666";
-    WIDGET_TEXT_COLOR = "#DDD";
-    WIDGET_SECONDARY_TEXT_COLOR = "#999";
+    WIDGET_BGCOLOR = "#222"
+    WIDGET_OUTLINE_COLOR = "#666"
+    WIDGET_TEXT_COLOR = "#DDD"
+    WIDGET_SECONDARY_TEXT_COLOR = "#999"
 
-    LINK_COLOR = "#9A9";
+    LINK_COLOR = "#9A9"
     // TODO: This is a workaround until LGraphCanvas.link_type_colors is no longer static.
-    static DEFAULT_EVENT_LINK_COLOR = "#A86";
-    EVENT_LINK_COLOR = "#A86";
-    CONNECTING_LINK_COLOR = "#AFA";
+    static DEFAULT_EVENT_LINK_COLOR = "#A86"
+    EVENT_LINK_COLOR = "#A86"
+    CONNECTING_LINK_COLOR = "#AFA"
 
-    MAX_NUMBER_OF_NODES = 10000; //avoid infinite loops
-    DEFAULT_POSITION = [100, 100]; //default node position
-    VALID_SHAPES = ["default", "box", "round", "card"]; //,"circle"
+    MAX_NUMBER_OF_NODES = 10000 //avoid infinite loops
+    DEFAULT_POSITION = [100, 100] //default node position
+    VALID_SHAPES = ["default", "box", "round", "card"] //,"circle"
 
 
     //shapes are used for nodes but also for slots
-    BOX_SHAPE = 1;
-    ROUND_SHAPE = 2;
-    CIRCLE_SHAPE = 3;
-    CARD_SHAPE = 4;
-    ARROW_SHAPE = 5;
-    GRID_SHAPE = 6; // intended for slot arrays
+    BOX_SHAPE = 1
+    ROUND_SHAPE = 2
+    CIRCLE_SHAPE = 3
+    CARD_SHAPE = 4
+    ARROW_SHAPE = 5
+    GRID_SHAPE = 6 // intended for slot arrays
 
 
     //enums
-    INPUT = 1;
-    OUTPUT = 2;
+    INPUT = 1
+    OUTPUT = 2
 
-    EVENT = -1; //for outputs
-    ACTION = -1; //for inputs
+    EVENT = -1 //for outputs
+    ACTION = -1 //for inputs
 
-    NODE_MODES = ["Always", "On Event", "Never", "On Trigger"]; // helper, will add "On Request" and more in the future
-    NODE_MODES_COLORS = ["#666", "#422", "#333", "#224", "#626"]; // use with node_box_coloured_by_mode
-    ALWAYS = 0;
-    ON_EVENT = 1;
-    NEVER = 2;
-    ON_TRIGGER = 3;
+    NODE_MODES = ["Always", "On Event", "Never", "On Trigger"] // helper, will add "On Request" and more in the future
+    NODE_MODES_COLORS = ["#666", "#422", "#333", "#224", "#626"] // use with node_box_coloured_by_mode
+    ALWAYS = 0
+    ON_EVENT = 1
+    NEVER = 2
+    ON_TRIGGER = 3
 
-    UP = 1;
-    DOWN = 2;
-    LEFT = 3;
-    RIGHT = 4;
-    CENTER = 5;
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+    CENTER = 5
 
-    LINK_RENDER_MODES = ["Straight", "Linear", "Spline"]; // helper
-    HIDDEN_LINK = -1;
-    STRAIGHT_LINK = 0;
-    LINEAR_LINK = 1;
-    SPLINE_LINK = 2;
+    LINK_RENDER_MODES = ["Straight", "Linear", "Spline"] // helper
+    HIDDEN_LINK = -1
+    STRAIGHT_LINK = 0
+    LINEAR_LINK = 1
+    SPLINE_LINK = 2
 
-    NORMAL_TITLE = 0;
-    NO_TITLE = 1;
-    TRANSPARENT_TITLE = 2;
-    AUTOHIDE_TITLE = 3;
-    VERTICAL_LAYOUT = "vertical"; // arrange nodes vertically
+    NORMAL_TITLE = 0
+    NO_TITLE = 1
+    TRANSPARENT_TITLE = 2
+    AUTOHIDE_TITLE = 3
+    VERTICAL_LAYOUT = "vertical" // arrange nodes vertically
 
-    proxy = null; //used to redirect calls
-    node_images_path = "";
+    proxy = null //used to redirect calls
+    node_images_path = ""
 
-    debug = false;
-    catch_exceptions = true;
-    throw_errors = true;
-    allow_scripts = false; //if set to true some nodes like Formula would be allowed to evaluate code that comes from unsafe sources (like node configuration), which could lead to exploits
-    registered_node_types = {}; //nodetypes by string
-    node_types_by_file_extension = {}; //used for dropping files in the canvas
-    Nodes = {}; //node types by classname
-    Globals = {}; //used to store vars between graphs
+    debug = false
+    catch_exceptions = true
+    throw_errors = true
+    allow_scripts = false //if set to true some nodes like Formula would be allowed to evaluate code that comes from unsafe sources (like node configuration), which could lead to exploits
+    registered_node_types = {} //nodetypes by string
+    node_types_by_file_extension = {} //used for dropping files in the canvas
+    Nodes = {} //node types by classname
+    Globals = {} //used to store vars between graphs
 
-    searchbox_extras = {}; //used to add extra features to the search box
-    auto_sort_node_types = false; // [true!] If set to true, will automatically sort node types / categories in the context menus
+    searchbox_extras = {} //used to add extra features to the search box
+    auto_sort_node_types = false // [true!] If set to true, will automatically sort node types / categories in the context menus
 
-    node_box_coloured_when_on = false; // [true!] this make the nodes box (top left circle) coloured when triggered (execute/action), visual feedback
-    node_box_coloured_by_mode = false; // [true!] nodebox based on node mode, visual feedback
+    node_box_coloured_when_on = false // [true!] this make the nodes box (top left circle) coloured when triggered (execute/action), visual feedback
+    node_box_coloured_by_mode = false // [true!] nodebox based on node mode, visual feedback
 
-    dialog_close_on_mouse_leave = false; // [false on mobile] better true if not touch device, TODO add an helper/listener to close if false
-    dialog_close_on_mouse_leave_delay = 500;
+    dialog_close_on_mouse_leave = false // [false on mobile] better true if not touch device, TODO add an helper/listener to close if false
+    dialog_close_on_mouse_leave_delay = 500
 
-    shift_click_do_break_link_from = false; // [false!] prefer false if results too easy to break links - implement with ALT or TODO custom keys
-    click_do_break_link_to = false; // [false!]prefer false, way too easy to break links
-    ctrl_alt_click_do_break_link = true; // [true!] who accidentally ctrl-alt-clicks on an in/output? nobody! that's who!
+    shift_click_do_break_link_from = false // [false!] prefer false if results too easy to break links - implement with ALT or TODO custom keys
+    click_do_break_link_to = false // [false!]prefer false, way too easy to break links
+    ctrl_alt_click_do_break_link = true // [true!] who accidentally ctrl-alt-clicks on an in/output? nobody! that's who!
 
-    search_hide_on_mouse_leave = true; // [false on mobile] better true if not touch device, TODO add an helper/listener to close if false
-    search_filter_enabled = false; // [true!] enable filtering slots type in the search widget, !requires auto_load_slot_types or manual set registered_slot_[in/out]_types and slot_types_[in/out]
-    search_show_all_on_open = true; // [true!] opens the results list when opening the search widget
+    search_hide_on_mouse_leave = true // [false on mobile] better true if not touch device, TODO add an helper/listener to close if false
+    search_filter_enabled = false // [true!] enable filtering slots type in the search widget, !requires auto_load_slot_types or manual set registered_slot_[in/out]_types and slot_types_[in/out]
+    search_show_all_on_open = true // [true!] opens the results list when opening the search widget
 
-    auto_load_slot_types = false; // [if want false, use true, run, get vars values to be statically set, than disable] nodes types and nodeclass association with node types need to be calculated, if dont want this, calculate once and set registered_slot_[in/out]_types and slot_types_[in/out]
+    auto_load_slot_types = false // [if want false, use true, run, get vars values to be statically set, than disable] nodes types and nodeclass association with node types need to be calculated, if dont want this, calculate once and set registered_slot_[in/out]_types and slot_types_[in/out]
 
 
     // set these values if not using auto_load_slot_types
-    registered_slot_in_types = {}; // slot types for nodeclass
-    registered_slot_out_types = {}; // slot types for nodeclass
-    slot_types_in = []; // slot types IN
-    slot_types_out = []; // slot types OUT
+    registered_slot_in_types = {} // slot types for nodeclass
+    registered_slot_out_types = {} // slot types for nodeclass
+    slot_types_in = [] // slot types IN
+    slot_types_out = [] // slot types OUT
     // @ts-expect-error
     slot_types_default_in: Record<string, string[]> = [] // specify for each IN slot type a(/many) default node(s), use single string, array, or object (with node, title, parameters, ..) like for search
     // @ts-expect-error
     slot_types_default_out: Record<string, string[]> = [] // specify for each OUT slot type a(/many) default node(s), use single string, array, or object (with node, title, parameters, ..) like for search
 
-    alt_drag_do_clone_nodes = false; // [true!] very handy, ALT click to clone and drag the new node
+    alt_drag_do_clone_nodes = false // [true!] very handy, ALT click to clone and drag the new node
 
-    do_add_triggers_slots = false; // [true!] will create and connect event slots when using action/events connections, !WILL CHANGE node mode when using onTrigger (enable mode colors), onExecuted does not need this
+    do_add_triggers_slots = false // [true!] will create and connect event slots when using action/events connections, !WILL CHANGE node mode when using onTrigger (enable mode colors), onExecuted does not need this
 
-    allow_multi_output_for_events = true; // [false!] being events, it is strongly reccomended to use them sequentially, one by one
+    allow_multi_output_for_events = true // [false!] being events, it is strongly reccomended to use them sequentially, one by one
 
-    middle_click_slot_add_default_node = false; //[true!] allows to create and connect a ndoe clicking with the third button (wheel)
+    middle_click_slot_add_default_node = false //[true!] allows to create and connect a ndoe clicking with the third button (wheel)
 
-    release_link_on_empty_shows_menu = false; //[true!] dragging a link to empty space will open a menu, add from list, search or defaults
+    release_link_on_empty_shows_menu = false //[true!] dragging a link to empty space will open a menu, add from list, search or defaults
 
-    pointerevents_method = "pointer"; // "mouse"|"pointer" use mouse for retrocompatibility issues? (none found @ now)
+    pointerevents_method = "pointer" // "mouse"|"pointer" use mouse for retrocompatibility issues? (none found @ now)
 
 
     // TODO implement pointercancel, gotpointercapture, lostpointercapture, (pointerover, pointerout if necessary)
-    ctrl_shift_v_paste_connect_unselected_outputs = true; //[true!] allows ctrl + shift + v to paste nodes with the outputs of the unselected nodes connected with the inputs of the newly pasted nodes
+    ctrl_shift_v_paste_connect_unselected_outputs = true //[true!] allows ctrl + shift + v to paste nodes with the outputs of the unselected nodes connected with the inputs of the newly pasted nodes
 
 
 
     // if true, all newly created nodes/links will use string UUIDs for their id fields instead of integers.
     // use this if you must have node IDs that are unique across all graphs and subgraphs.
-    use_uuids = false;
+    use_uuids = false
 
     // Whether to highlight the bounding box of selected groups
     highlight_selected_group = false
@@ -303,7 +303,7 @@ export class LiteGraphGlobal {
         if (this.auto_load_slot_types) {
             new base_class(base_class.title || "tmpnode")
         }
-    };
+    }
 
     onNodeTypeRegistered?(type: string, base_class: typeof LGraphNode): void
     onNodeTypeReplaced?(type: string, base_class: typeof LGraphNode, prev: unknown): void
@@ -324,7 +324,7 @@ export class LiteGraphGlobal {
         if (base_class.constructor.name) {
             delete this.Nodes[base_class.constructor.name]
         }
-    };
+    }
 
     /**
     * Save a slot type and his node
@@ -380,7 +380,7 @@ export class LiteGraphGlobal {
                 }
             }
         }
-    };
+    }
 
     /**
      * Create a new nodetype by passing a function, it wraps it with a proper class and generates inputs according to the parameters of the function.
@@ -435,7 +435,7 @@ export class LiteGraphGlobal {
         }
         // @ts-expect-error Required to make this kludge work
         this.registerNodeType(name, classobj)
-    };
+    }
 
     /**
      * Removes all previously registered node's types
@@ -445,7 +445,7 @@ export class LiteGraphGlobal {
         this.node_types_by_file_extension = {}
         this.Nodes = {}
         this.searchbox_extras = {}
-    };
+    }
 
     /**
      * Adds this method to all nodetypes, existing and to be created
@@ -463,7 +463,7 @@ export class LiteGraphGlobal {
             } //keep old in case of replacing
             type.prototype[name] = func
         }
-    };
+    }
 
     /**
      * Create a node of a given type with a name. The node is not attached to any graph yet.
@@ -538,7 +538,7 @@ export class LiteGraphGlobal {
         }
 
         return node
-    };
+    }
 
     /**
      * Returns a registered node type with a given name
@@ -548,7 +548,7 @@ export class LiteGraphGlobal {
      */
     getNodeType(type: string): typeof LGraphNode {
         return this.registered_node_types[type]
-    };
+    }
 
     /**
      * Returns a list of node types matching one category
@@ -578,7 +578,7 @@ export class LiteGraphGlobal {
         }
 
         return r
-    };
+    }
 
     /**
      * Returns a list with all the node type categories
@@ -601,7 +601,7 @@ export class LiteGraphGlobal {
             result.push(i)
         }
         return this.auto_sort_node_types ? result.sort() : result
-    };
+    }
 
     //debug purposes: reloads all the js scripts that matches a wildcard
     reloadNodes(folder_wildcard: string): void {
@@ -644,7 +644,7 @@ export class LiteGraphGlobal {
         if (this.debug) {
             console.log("Nodes reloaded")
         }
-    };
+    }
 
     //separated just to improve if it doesn't work
     cloneObject<T extends object>(obj: T, target?: T): T {
@@ -660,7 +660,7 @@ export class LiteGraphGlobal {
             target[i] = r[i]
         }
         return target
-    };
+    }
 
     /*
         * https://gist.github.com/jed/982883?permalink_comment_id=852670#gistcomment-852670
@@ -668,7 +668,7 @@ export class LiteGraphGlobal {
     uuidv4(): string {
         // @ts-ignore
         return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, a => (a ^ Math.random() * 16 >> a / 4).toString(16))
-    };
+    }
 
     /**
      * Returns if the types of two slots are compatible (taking into account wildcards, etc)
@@ -711,7 +711,7 @@ export class LiteGraphGlobal {
         }
 
         return false
-    };
+    }
 
     /**
      * Register a string in the search box so when the user types it it will recommend this node
@@ -727,7 +727,7 @@ export class LiteGraphGlobal {
             desc: description,
             data: data
         }
-    };
+    }
 
     /**
      * Wrapper to load files (from url using fetch or from file using FileReader)
@@ -789,7 +789,7 @@ export class LiteGraphGlobal {
                 return reader.readAsBinaryString(url)
         }
         return null
-    };
+    }
 
     //used to create nodes from wrapping functions
     getParameterNames(func: Function): string[] {
@@ -802,7 +802,7 @@ export class LiteGraphGlobal {
             .replace(/=[^,]+/g, "") // strip any ES6 defaults
             .split(",")
             .filter(Boolean) // split & filter [""]
-    };
+    }
 
     /* helper for interaction: pointer, touch, mouse Listeners
     used by LGraphCanvas DragAndScale ContextMenu*/
