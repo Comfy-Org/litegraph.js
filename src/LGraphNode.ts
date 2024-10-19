da4e54966e48f5bfd9eff278db361d233bcaef5c
filtered <176114999+webfiltered@users.noreply.github.com>
2024-10-20 04:48:45 +1100

    nit

--- src/LGraphNode.ts
+++ src/LGraphNode.ts
@@ -131,7 +131,7 @@ export class LGraphNode {
     connections: unknown[]
     properties: INodeProperties
     properties_info: INodePropertyInfo[]
-    flags?: INodeFlags
+    flags: INodeFlags
     widgets?: IWidget[]
 
     size: Point
@@ -1478,7 +1478,7 @@ export class LGraphNode {
             ? 0
             : LiteGraph.NODE_TITLE_HEIGHT
 
-        if (this.flags?.collapsed) {
+        if (this.flags.collapsed) {
             //if ( distance([x,y], [this.pos[0] + this.size[0]*0.5, this.pos[1] + this.size[1]*0.5]) < LiteGraph.NODE_COLLAPSED_RADIUS)
             if (isInsideRectangle(
                 x,
