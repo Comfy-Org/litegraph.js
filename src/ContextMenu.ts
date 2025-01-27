d91ecaa86c671aea272844c3900a18da1af7bf01
christian-byrne <abolkonsky.rem@gmail.com>
2025-01-26 18:51:31 -0700

    remove scaling of context menus based on graph scale

--- src/ContextMenu.ts
+++ src/ContextMenu.ts
@@ -180,8 +180,6 @@ export class ContextMenu {
 
     root.style.left = left + "px"
     root.style.top = top + "px"
-
-    if (options.scale) root.style.transform = `scale(${Math.round(options.scale * 4) * 0.25})`
   }
 
   addItem(
