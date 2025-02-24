74b641d2d34a13ed3543b7dd51984bd747abf0d1
filtered <176114999+webfiltered@users.noreply.github.com>
2025-02-24 12:43:30 +1100

    Split test graphs out to separate file

--- test/testExtensions.ts
+++ test/testExtensions.ts
@@ -1,68 +1,7 @@
 import { test as baseTest } from "vitest"
 import type { ISerialisedGraph, SerialisableGraph } from "../src/types/serialisation"
 import { LGraph } from "@/LGraph"
-
-const oldSchemaGraph: ISerialisedGraph = {
-  version: 0.4,
-  config: {},
-  last_node_id: 0,
-  last_link_id: 0,
-  groups: [
-    {
-      id: 123,
-      bounding: [20, 20, 1, 3],
-      color: "#6029aa",
-      font_size: 14,
-      title: "A group to test with",
-    },
-  ],
-  nodes: [
-    {
-      id: 1,
-    },
-  ],
-  links: [],
-}
-
-const minimalSerialisableGraph: SerialisableGraph = {
-  version: 1,
-  config: {},
-  state: {
-    lastNodeId: 0,
-    lastLinkId: 0,
-    lastGroupId: 0,
-    lastRerouteId: 0
-  },
-  nodes: [],
-  links: [],
-  groups: [],
-}
-
-const basicSerialisableGraph: SerialisableGraph = {
-  version: 1,
-  config: {},
-  state: {
-    lastNodeId: 0,
-    lastLinkId: 0,
-    lastGroupId: 0,
-    lastRerouteId: 0
-  },
-  groups: [
-    {
-      id: 123,
-      bounding: [20, 20, 1, 3],
-      color: "#6029aa",
-      font_size: 14,
-      title: "A group to test with",
-    },
-  ],
-  nodes: [
-    {
-      id: 1,
-    },
-  ],
-  links: [],
-}
+import { basicSerialisableGraph, minimalSerialisableGraph, oldSchemaGraph } from "./assets/testGraphs"
 
 interface LitegraphFixtures {
   minimalGraph: LGraph
