35ce7dabd844a911f3385d0a84714ba5ca1bc219
filtered <176114999+webfiltered@users.noreply.github.com>
2025-02-26 23:35:50 +1100

    [TS] Add ts-expect-error to widget map

--- src/widgets/widgetMap.ts
+++ src/widgets/widgetMap.ts
@@ -1,4 +1,3 @@
-// @ts-strict-ignore
 import type { IBaseWidget } from "@/types/widgets"
 
 import { BaseWidget } from "./BaseWidget"
@@ -14,11 +13,18 @@ type WidgetConstructor = {
 }
 
 export const WIDGET_TYPE_MAP: Record<string, WidgetConstructor> = {
+  // @ts-expect-error https://github.com/Comfy-Org/litegraph.js/issues/616
   button: ButtonWidget,
+  // @ts-expect-error #616
   toggle: BooleanWidget,
+  // @ts-expect-error #616
   slider: SliderWidget,
+  // @ts-expect-error #616
   combo: ComboWidget,
+  // @ts-expect-error #616
   number: NumberWidget,
+  // @ts-expect-error #616
   string: TextWidget,
+  // @ts-expect-error #616
   text: TextWidget,
 }
