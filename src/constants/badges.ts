ad81449485a836879728df69499f89f3a13bf913
bymyself <cbyrne@comfy.org>
2025-04-15 16:18:52 -0700

    add badge and fonts configs

---
+++ src/constants/badges.ts
@@ -0,0 +1,20 @@
+import { PRIME_ICONS_UNICODES } from './fonts';
+export type PrimeIconKey = keyof typeof PRIME_ICONS_UNICODES;
+
+export interface BadgeConfig {
+  icon: PrimeIconKey;
+  badgeBg: string;
+  badgeText: string;
+  iconBg: string;
+  iconColor: string;
+}
+
+export const BADGE_CONFIGS: Record<string, BadgeConfig> = {
+  credits: {
+    icon: 'pi-dollar',
+    badgeBg: '#8D6932',
+    badgeText: '#fff',
+    iconBg: '#654020',
+    iconColor: '#FABC25',
+  },
+};
---
+++ src/constants/fonts.ts
@@ -0,0 +1,4 @@
+export const PRIME_ICONS_FONT_FAMILY = 'PrimeIcons'
+export const PRIME_ICONS_UNICODES = {
+  'pi-dollar': '\ue96b',
+}
