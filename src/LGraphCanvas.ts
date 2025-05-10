b15e1104677e470b5a89dfb52be731e3c141fb47
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-11 04:20:47 +1000

    Add convert to subgraph context menu

--- src/LGraphCanvas.ts
+++ src/LGraphCanvas.ts
@@ -7175,6 +7175,12 @@ export class LGraphCanvas {
         content: "Distribute Nodes",
         has_submenu: true,
         callback: LGraphCanvas.createDistributeMenu,
+      }, {
+        content: "Convert to Subgraph",
+        callback: () => {
+          if (!this.graph) throw new NullGraphError()
+          this.graph.convertToSubgraph(this.selectedItems)
+        },
       })
     }
 
